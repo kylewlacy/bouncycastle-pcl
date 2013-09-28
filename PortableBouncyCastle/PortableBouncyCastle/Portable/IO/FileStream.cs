1c499440da353a814df7b15d7c817213bf61c807
Kyle Lacy <kylewlacy@me.com>
2013-09-27 19:54:06 -0700

    Use `TikoContainer` to resolve `RNGCryptoServiceProvider` and `FileStream`

--- PortableBouncyCastle/PortableBouncyCastle/Portable/IO/FileStream.cs
+++ PortableBouncyCastle/PortableBouncyCastle/Portable/IO/FileStream.cs
@@ -1,12 +1,18 @@
 using System;
 using System.IO;
+using Tiko;
 
 namespace Org.BouncyCastle.Portable.IO
 {
 	public abstract class FileStream : Stream
 	{
-		public static FileStream Create(string path, FileMode mode, FileAccess access, FileShare share, int bufferSize) {
-			throw new NotImplementedException ();
+		protected abstract void Initialize (string path, FileMode mode, FileAccess access, FileShare share, int bufferSize);
+
+		public static FileStream Create (string path, FileMode mode, FileAccess access, FileShare share, int bufferSize)
+		{
+			var fileStream = TikoContainer.Resolve<FileStream> ();
+			fileStream.Initialize (path, mode, access, share, bufferSize);
+			return fileStream;
 		}
 	}
 }
--- PortableBouncyCastle/PortableBouncyCastle/Portable/Security/Cryptography/RNGCryptoServiceProvider.cs
+++ PortableBouncyCastle/PortableBouncyCastle/Portable/Security/Cryptography/RNGCryptoServiceProvider.cs
@@ -1,4 +1,5 @@
 using System;
+using Tiko;
 
 namespace Org.BouncyCastle.Portable.Security.Cryptography
 {
@@ -6,9 +7,13 @@ namespace Org.BouncyCastle.Portable.Security.Cryptography
 	{
 		public abstract void GetBytes (byte[] data);
 
+		protected abstract void Initialize ();
+
 		public static RNGCryptoServiceProvider Create ()
 		{
-			throw new NotImplementedException ();
+			var cryptoServiceProvider = TikoContainer.Resolve<RNGCryptoServiceProvider> ();
+			cryptoServiceProvider.Initialize ();
+			return cryptoServiceProvider;
 		}
 	}
 }
